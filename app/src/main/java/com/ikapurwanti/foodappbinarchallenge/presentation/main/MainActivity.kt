979148fc421d87296aa565b4d682db2477a5fcf1
Ikap <ikapurwanti144@gmail.com>
2023-09-28 18:14:23 +0700

    prepare base project

---
+++ app/src/main/java/com/ikapurwanti/foodappbinarchallenge/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ikapurwanti.foodappbinarchallenge.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ikapurwanti.foodappbinarchallenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
