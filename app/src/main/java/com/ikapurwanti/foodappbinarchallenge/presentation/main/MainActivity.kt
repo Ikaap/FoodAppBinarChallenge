34c5e97f91efdf9ae58eccdc57d90be280c5d498
Ikap <ikapurwanti144@gmail.com>
2023-09-30 22:16:35 +0700

    style
    ui home fragment

--- app/src/main/java/com/ikapurwanti/foodappbinarchallenge/presentation/main/MainActivity.kt
+++ app/src/main/java/com/ikapurwanti/foodappbinarchallenge/presentation/main/MainActivity.kt
@@ -2,11 +2,28 @@ package com.ikapurwanti.foodappbinarchallenge.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.ikapurwanti.foodappbinarchallenge.R
+import com.ikapurwanti.foodappbinarchallenge.databinding.ActivityMainBinding
+import com.ikapurwanti.foodappbinarchallenge.presentation.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding : ActivityMainBinding
+
+    private val fragmentHome = HomeFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadFragment(fragmentHome)
+    }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_dashboard, fragment)
+            .commit()
+
     }
 }
