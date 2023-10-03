33c79fad722c0179383b2cc4ae8ca55ed6185f0e
Ikap <ikapurwanti144@gmail.com>
2023-10-04 03:07:48 +0700

    finishing layout checkout

---
+++ app/src/main/java/com/ikapurwanti/foodappbinarchallenge/presentation/checkout/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.ikapurwanti.foodappbinarchallenge.presentation.checkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ikapurwanti.foodappbinarchallenge.R
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
