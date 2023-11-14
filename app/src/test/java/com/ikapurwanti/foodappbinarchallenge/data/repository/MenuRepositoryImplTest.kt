375463e7bdaa12be75a671a5f8f4b6f4a89a1778
Ikap <ikapurwanti144@gmail.com>
2023-11-14 09:12:13 +0700

    add unit testing menu repository

---
+++ app/src/test/java/com/ikapurwanti/foodappbinarchallenge/data/repository/MenuRepositoryImplTest.kt
@@ -0,0 +1,21 @@
+package com.ikapurwanti.foodappbinarchallenge.data.repository
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class MenuRepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getCategories() {
+    }
+
+    @Test
+    fun getMenu() {
+    }
+}
